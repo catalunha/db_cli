55f4f041c01b02752cbf9ef985a2c5508e4b5f4c
catalunha <catalunha.mj@gmail.com>
2023-05-17 06:47:21 -0300

    first commit

---
+++ bin/db_cli.dart
@@ -0,0 +1,5 @@
+import 'package:db_cli/db_cli.dart' as db_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${db_cli.calculate()}!');
+}
---
+++ test/db_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:db_cli/db_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
