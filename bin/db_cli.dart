424719550ee51bc9f6b5694ec78164f5fe2b2a8f
catalunha <catalunha.mj@gmail.com>
2023-05-17 09:45:56 -0300

    comandos de terminal no dart

--- bin/db_cli.dart
+++ bin/db_cli.dart
@@ -1,5 +1,32 @@
-import 'package:db_cli/db_cli.dart' as db_cli;
+import 'package:args/command_runner.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${db_cli.calculate()}!');
+  // print(arguments);
+  // final argParser = ArgParser();
+  // argParser.addFlag('data', abbr: 'd');
+  // argParser.addOption('name', abbr: 'n');
+  // argParser.addOption('type', abbr: 't');
+  // final argResult = argParser.parse(arguments);
+  // print(argResult['data']);
+  // print(argResult['name']);
+  // print(argResult['type']);
+  CommandRunner('DB cli', 'DB cli...')
+    ..addCommand(ExemploCommand())
+    ..run(arguments);
+}
+
+class ExemploCommand extends Command {
+  @override
+  String get name => 'exemplo';
+  @override
+  String get description => 'Exemplo de comando';
+
+  ExemploCommand() {
+    argParser.addOption('type', abbr: 't', help: 'Envie o tipo de programa');
+  }
+  @override
+  void run() {
+    print(argResults?['type']);
+    print('exe...');
+  }
 }
--- test/db_cli_test.dart
+++ test/db_cli_test.dart
@@ -1,8 +1,7 @@
-import 'package:db_cli/db_cli.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    expect(calculate(), 42);
+    // expect(calculate(), 42);
   });
 }
